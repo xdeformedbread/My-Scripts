9dab823055b55b1909249a7e93c29a987e9807fe
xdeformedbread <83147437+xdeformedbread@users.noreply.github.com>
2022-08-04 10:20:24 -0500

    Add files via upload

---
+++ NEW EVERYTHING.lua
@@ -0,0 +1,11 @@
+if game.PlaceId == 9598746251 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Home%20Run%20Sim.lua"))()
+    elseif game.PlaceId == 5490351219 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Clicker%20Madness.lua"))()
+    elseif game.PlaceId == 3101667897 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Legend%20of%20speed.lua"))()
+    else return
+end
+
+
+

b03a17c0c6cb376181ecda46bc78b8a6776fe4e6
xdeformedbread <83147437+xdeformedbread@users.noreply.github.com>
2022-09-07 22:13:00 -0500

    Update NEW EVERYTHING.lua

--- NEW EVERYTHING.lua
+++ NEW EVERYTHING.lua
@@ -6,5 +6,7 @@ if game.PlaceId == 9598746251 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Legend%20of%20speed.lua"))()
     elseif game.PlaceId == 8884334497 then
         loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/mining%20clicker%20simulator.lua"))()
+    elseif game.PlaceId == 8612195438 then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Clicker%20Party%20sim.lua"))()
     else return
 end

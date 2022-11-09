27dc83de801c3fef230494672b9fccd33b744193
xdeformedbread <83147437+xdeformedbread@users.noreply.github.com>
2022-11-09 16:49:26 -0600

    Update and rename Rasberry Hub.lua to Raspberry Hub.lua

--- Rasberry Hub.lua
+++
@@ -1,8 +0,0 @@
-if game.PlaceId == 8884334497 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/min%20click%20sim.lua"))()
-    elseif game.PlaceId == 9598746251 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/home%20run%20sim.lua"))()
-    elseif game.PlaceId == 5490351219 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/click%20mad.lua"))()
-    else return
-end
---
+++ Raspberry Hub.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 6679968919 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Fly%20Race.lua"))()
+    elseif game.PlaceId == 8884334497 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/min%20click%20sim.lua"))()
+    else return
+end

4515319a09816ecca3c3c70adf64d0ab128dd2da
xdeformedbread <83147437+xdeformedbread@users.noreply.github.com>
2022-11-13 16:32:04 -0600

    Update Raspberry Hub.lua

--- Raspberry Hub.lua
+++ Raspberry Hub.lua
@@ -4,5 +4,7 @@ if game.PlaceId == 6679968919 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/min%20click%20sim.lua"))()
     elseif game.PlaceId == 3101667897 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Legends%20Of%20Speed.lua"))()
+    elseif game.PlaceId == 9598746251 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/xdeformedbread/My-Scripts/main/Home%20Run%20Sim.lua"))()
     else return
 end
